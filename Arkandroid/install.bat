bb59ee34708e5193161c8a369f8abb8b5b090d7c
kikemadrigal <kikemadrigal@hotmail.com>
2026-07-25 07:21:07 +0200

    updated arkandroid

--- Arkandroid/install.bat
+++ Arkandroid/install.bat
@@ -1,3 +1,5 @@
+SET ECHO ON
+
 rem comprimimos todos los archivos en ua app con ziptool
 CD ./app
 ZIPTOOL /C /Y Arkandroid.app 
@@ -9,18 +11,18 @@ move ./app/Arkandroid.app $SYSTEM:/apps/Arkandroid.app
 rem Copiamos la base ded datos a la carpeta de aplicaciones
 rem copy ./app/arkandroid.db $SYSTEM:/apps/arkandroid.db
 
-rem Creamos el archivo lanzador
-echo "[Desktop Entry]" > Arkandroid.desktop
-echo "Version=1.4" >> Arkandroid.desktop
-echo "Terminal=false" >> Arkandroid.desktop
-echo "Type=Application" >> Arkandroid.desktop
-echo "Name=Arkandroid" >> Arkandroid.desktop
-echo "Exec=$SYSTEM:/apps/Arkandroid.app" >> Arkandroid.desktop
-echo "Icon=$SYSTEM:/apps/Arkandroid.app/app.png" >> Arkandroid.desktop
-echo "Categories=Games" >> Arkandroid.desktop
-echo "Keywords=arkandroid;Arkandroid" >> Arkandroid.desktop
-echo "."
-
-rem Creamos el lanzador o acceso directo o lin del escritorio
-copy Arkandroid.desktop $SYSTEM:/user/desktop/Arkandroid
-move Arkandroid.desktop $SYSTEM:/desktop/Arkandroid
+rem rem Creamos el archivo lanzador
+rem echo "[Desktop Entry]" > Arkandroid.desktop
+rem echo "Version=1.4" >> Arkandroid.desktop
+rem echo "Terminal=false" >> Arkandroid.desktop
+rem echo "Type=Application" >> Arkandroid.desktop
+rem echo "Name=Arkandroid" >> Arkandroid.desktop
+rem echo "Exec=$SYSTEM:/apps/Arkandroid.app" >> Arkandroid.desktop
+rem echo "Icon=$SYSTEM:/apps/Arkandroid.app/app.png" >> Arkandroid.desktop
+rem echo "Categories=Games" >> Arkandroid.desktop
+rem echo "Keywords=arkandroid;Arkandroid" >> Arkandroid.desktop
+rem echo "."
+rem 
+rem rem Creamos el lanzador o acceso directo o lin del escritorio
+rem copy Arkandroid.desktop $SYSTEM:/user/desktop/Arkandroid
+rem move Arkandroid.desktop $SYSTEM:/desktop/Arkandroid
